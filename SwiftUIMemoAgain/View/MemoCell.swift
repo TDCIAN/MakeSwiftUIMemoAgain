0d9e6cadf2ecad92e8fc4dbf9e9c3082f6d5f0ef
Jeongmin <tdcian71@gmail.com>
2021-05-13 17:54:02 +0900

    메모 리스트 리팩토링

---
+++ SwiftUIMemoAgain/View/MemoCell.swift
@@ -0,0 +1,32 @@
+//
+//  MemoCell.swift
+//  SwiftUIMemoAgain
+//
+//  Created by APPLE on 2021/05/13.
+//
+
+import SwiftUI
+
+struct MemoCell: View {
+    @ObservedObject var memo: Memo
+    @EnvironmentObject var formatter: DateFormatter
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(memo.content)
+                .font(.body)
+                .lineLimit(1)
+            
+            Text("\(memo.insertDate, formatter: self.formatter)")
+                .font(.caption)
+                .foregroundColor(Color(UIColor.secondaryLabel))
+        }
+    }
+}
+
+struct MemoCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoCell(memo: Memo(content: "Test"))
+            .environmentObject(DateFormatter.memoDateFormatter)
+    }
+}
