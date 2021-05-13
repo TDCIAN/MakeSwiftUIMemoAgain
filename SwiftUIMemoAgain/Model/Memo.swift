3637c9c696b961390c042464fadc57e40b2a52a6
Jeongmin <tdcian71@gmail.com>
2021-05-13 17:32:49 +0900

    Model 추가

---
+++ SwiftUIMemoAgain/Model/Memo.swift
@@ -0,0 +1,26 @@
+//
+//  Memo.swift
+//  SwiftUIMemoAgain
+//
+//  Created by APPLE on 2021/05/13.
+//
+
+import SwiftUI
+
+class Memo: Identifiable, ObservableObject {
+    let id: UUID
+    @Published var content: String
+    let insertDate: Date
+    
+    init(id: UUID = UUID(), content: String, insertDate: Date = Date()) {
+        self.id = id
+        self.content = content
+        self.insertDate = insertDate
+    }
+}
+
+extension Memo: Equatable {
+    static func == (lhs: Memo, rhs: Memo) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
