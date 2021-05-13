5bc20ed74150a87ffc01923ce320f8204e53a5c2
Jeongmin <tdcian71@gmail.com>
2021-05-13 17:18:37 +0900

    Initial Commit

---
+++ SwiftUIMemoAgain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMemoAgain
+//
+//  Created by APPLE on 2021/05/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
